b6535fab64a10e7f1642da1e04b06d50ea8b6b0b
Fabiola <fabiola.aurelien@gmail.com>
2026-04-09 21:18:53 -0400

    I

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+// Taken from Chat to create my toggle button
 const themeToggle = document.getElementById("themeToggle") as HTMLInputElement;
 const root = document.documentElement;
 
@@ -5,17 +6,52 @@ const root = document.documentElement;
 const savedTheme = localStorage.getItem("theme");
 
 if (savedTheme === "dark") {
-  root.setAttribute("data-bs-theme", "dark");
-  themeToggle.checked = true;
+    root.setAttribute("data-bs-theme", "dark");
+    themeToggle.checked = true;
 } else {
-  root.setAttribute("data-bs-theme", "light");
-  themeToggle.checked = false;
+    root.setAttribute("data-bs-theme", "light");
+    themeToggle.checked = false;
 }
 
 // Toggle theme when switch changes
 themeToggle.addEventListener("change", () => {
-  const newTheme = themeToggle.checked ? "dark" : "light";
+    const newTheme = themeToggle.checked ? "dark" : "light";
 
-  root.setAttribute("data-bs-theme", newTheme);
-  localStorage.setItem("theme", newTheme);
+    root.setAttribute("data-bs-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
 });
+
+async function getFlags() {
+    const response = await fetch("https://restcountries.com/v3.1/name/haiti");
+    const result = await response.json();
+
+    const container = document.getElementById("countryFlags");
+
+    if (!container) return;
+
+    const country = result[0];
+
+    //  Create the column div
+    const col = document.createElement("div");
+    col.classList.add("col-3");
+
+    // Create the image
+    const img = document.createElement("img");
+    img.src = country.flags.svg;
+    img.alt = country.name.common;
+    img.classList.add("img-fluid");
+
+    // Put image inside div
+    col.appendChild(img);
+
+    // Put div into container
+    container.appendChild(col);
+}
+
+getFlags();
+
+
+getFlags();
+
+getFlags();
+getFlags();
